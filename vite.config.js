822d8874426b4815479fca7ff3ab7de96f17916b
UmarFarooq7432 <farooqumar8286@gmail.com>
2025-06-08 11:58:12 +0530

    Fix: updated vite.config.js base path for Render

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/quote-generator/',
   plugins: [react()],
+  base: './', // ✅ ADD THIS LINE
 })
